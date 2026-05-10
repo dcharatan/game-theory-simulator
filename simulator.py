f48b1f34c72cbecb3be13004929aae58378ce20d
David Charatan <david.charatan@gmail.com>
2026-05-10 18:40:47 -0400

    WIP something is wrong

--- simulator.py
+++ simulator.py
@@ -1,5 +1,4 @@
-from dataclasses import dataclass, replace
-from itertools import permutations
+from dataclasses import dataclass
 
 import numpy as np
 from jaxtyping import Float
@@ -29,36 +28,26 @@ class Game:
 # State and Action Definitions #
 ################################
 
-# For ChanceState, None is the only valid action. It essentially means "pick among the
-# possible states at random." For DecisionState, None means do nothing, and an int means
-# switching to that release date.
+# None means do nothing, and an int means switching to that release date.
 type Action = int | None
 
 
 @dataclass(frozen=True)
-class ChanceState:
-    week: int  # for week == num_weeks, this is a terminal state
-    releases: tuple[int | None, ...]
-
-
-@dataclass(frozen=True)
-class DecisionState:
-    week: int
-    ordering: tuple[int, ...]
+class State:
     turn: int
     releases: tuple[int | None, ...]
 
-    def __post_init__(self) -> None:
-        num_players = len(self.ordering)
-        assert self.turn < num_players
-        assert len(self.releases) == num_players
+    @property
+    def num_players(self) -> int:
+        return len(self.releases)
 
     @property
     def current_player(self) -> int:
-        return self.ordering[self.turn]
+        return self.turn % self.num_players
 
-
-type State = ChanceState | DecisionState
+    @property
+    def week(self) -> int:
+        return self.turn // self.num_players
 
 
 ################################
@@ -67,17 +56,17 @@ type State = ChanceState | DecisionState
 
 
 def initial_state(game: Game) -> State:
-    return ChanceState(0, (None,) * game.num_players)
+    return State(0, (None,) * game.num_players)
 
 
 def valid_actions(game: Game, state: State) -> tuple[Action, ...]:
-    # For ChanceState, the only valid action is to sample (represented as None).
-    if isinstance(state, ChanceState):
-        return (None,)
+    # Check whether the game is over.
+    if state.week == game.num_weeks:
+        return ()
 
-    # For DecisionState, valid actions are do nothing (None) or pick a new week (int).
     planned_release = state.releases[state.current_player]
     if planned_release is not None and state.week > planned_release:
+        # If the release has already happened, it can't be changed.
         return (None,)
     else:
         # If the release hasn't happened yet, it can be changed.
@@ -85,39 +74,23 @@ def valid_actions(game: Game, state: State) -> tuple[Action, ...]:
         return (None, *[w for w in weeks if w != planned_release])
 
 
-def advance(game: Game, state: State, action: Action) -> tuple[State, ...]:
-    if isinstance(state, ChanceState):
-        # Return all possible turn orderings for the next week.
-        possibilities = [
-            DecisionState(state.week, tuple(order), 0, state.releases)
-            for order in permutations(range(game.num_players))
-        ]
-        return tuple(possibilities)
-
-    # Update the releases and switches based on the action.
+def advance(state: State, action: Action) -> State:
+    # Update the releases based on the action.
     releases = list(state.releases)
     if action is not None:
         releases[state.current_player] = action
-    releases = tuple(releases)
+    return State(state.turn + 1, tuple(releases))
 
-    if state.turn == game.num_players - 1:
-        # If all players have gone, we move to the next week.
-        return (ChanceState(state.week + 1, releases),)
-    else:
-        # Otherwise, we advance to the next player.
-        return (replace(state, turn=state.turn + 1, releases=releases),)
+
+type Reward = Float[np.ndarray, " player"]
 
 
 def compute_reward(
     game: Game,
     old: State,
     new: State,
-) -> Float[np.ndarray, " player"]:
+) -> Reward:
     reward = np.zeros((game.num_players,), dtype=np.float32)
-
-    # ChanceState does not create rewards.
-    if isinstance(old, ChanceState):
-        return reward
 
     player = old.current_player
     attractiveness = game.attractivenesses[player]
@@ -146,16 +119,50 @@ def compute_reward(
 
     return reward
 
+
 ################
 # Optimization #
 ################
 
+type Cache = dict[State, tuple[Reward, Action]]
 
-def compute_optimal(
+
+def optimize(
     game: Game,
     state: State,
-):
-    pass
+    cache: Cache,
+) -> tuple[Reward, Action]:
+    if state in cache:
+        return cache[state]
+
+    best_value = np.zeros((game.num_players,), dtype=np.float32)
+    best_action = None
+
+    for action in valid_actions(game, state):
+        new = advance(state, action)
+        reward = compute_reward(game, state, new)
+        new_value, _ = optimize(game, new, cache)
+        old_value = reward + new_value
+        p = state.current_player
+        if best_action is None or old_value[p] > best_value[p]:
+            best_value = old_value
+            best_action = action
+
+    cache[state] = (best_value, best_action)
+
+    return best_value, best_action
+
+
+def play(game: Game) -> tuple[Action, ...]:
+    cache = {}
+    state = initial_state(game)
+    actions = []
+    while valid_actions(game, state):
+        _, best_action = optimize(game, state, cache)
+        state = advance(state, best_action)
+        actions.append(best_action)
+    return tuple(actions)
+
 
 if __name__ == "__main__":
     game = Game(
@@ -164,11 +171,6 @@ if __name__ == "__main__":
         np.array((-0.01, 0.01, 0.01, 0.01)),  # best to release after waiting one week
         -0.1,  # cost of switching
     )
-    old = initial_state(game)
-    new = advance(game, old, valid_actions(game, old)[0])[0]
-    reward = compute_reward(game, old, new)
-    old = new
-    new = advance(game, old, 0)[0]
-    reward = compute_reward(game, old, new)
 
+    actions = play(game)
     a = 1
